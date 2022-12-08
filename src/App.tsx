15de0a88b2267d6eccf7d3edd579c863735f6e0b
Jess2410 <jess2410@hotmail.fr>
2022-12-08 17:59:13 +0100

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Home from "./pages/Home";
+
+import Navbar from "./components/Navbar";
+import Store from "./pages/Store";
+import About from "./pages/About";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+// import { Home } from "./pages/Home"
+// import { Store } from "./pages/Store"
+// import { About } from "./pages/About"
+
+// import { ShoppingCartProvider } from "./context/ShoppingCartContext"
 
 function App() {
   return (
-    <div className="App">
-      <h1>coucou</h1>
-    </div>
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
   );
 }
 
